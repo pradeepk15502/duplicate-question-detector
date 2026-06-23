6b43c8b4ee8ca56dcbe6f8f058b9420ae5b8af86
Pradeep Kumar <168911450+pradeepk15502@users.noreply.github.com>
2026-06-23 09:24:01 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import streamlit as st
 import helper
 import pickle
 
+import os
+import zipfile
+
+if not os.path.exists("model.pkl"):
+    with zipfile.ZipFile("model.zip", "r") as zip_ref:
+        zip_ref.extractall(".")
+        
 model = pickle.load(open('model.pkl','rb'))
 
 st.header('Duplicate Question Pairs')
